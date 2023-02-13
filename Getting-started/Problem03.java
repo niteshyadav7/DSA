a1cbaef5d949b59887891c6379c99c172daff9fe
niteshyadav7 <nitesh0709yadav@gmail.com>
2023-02-13 13:07:44 +0530

    getting started

---
+++ Getting-started/Problem03.java
@@ -0,0 +1,12 @@
+public class Problem03 {
+    public static void main(String[] args) {
+        int x=10;
+        int y=15;
+        int sum =x+y;
+        int product=x*y;
+        int mod=x%y;
+        System.out.println(sum);
+        System.out.println(product);
+        System.out.println(mod);
+    }
+}
