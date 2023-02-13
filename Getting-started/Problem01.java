d301bd315f610ce7b746bd170f521a572ddd1b1f
niteshyadav7 <nitesh0709yadav@gmail.com>
2023-02-13 12:21:50 +0530

    print triangle hardCode

---
+++ Getting-started/Problem01.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Problem01 {
+    public static void main(String[] args) {
+        System.out.println("   *");
+        System.out.println("  ***");
+        System.out.println(" *****");
+        System.out.println("********");
+    }
+}
